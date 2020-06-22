e737def3a0faf759ff9c153aca49c14e513cf986
quyawei <quyawei1@outlook.com>
2020-06-22 19:31:50 +0800

    表单验证自定义规则-基于函数

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,23 @@
 $(function () {
+    // layui是全局对象，通过它可以得到form对象
+    var form = layui.form
+    // 基于LayUI自定义表单验证规则
+    form.verify({
+        // 必须是6-8位字符，不包括空格
+        uname: [/^[\S]{6,8}$/, '用户名必须是6-8位字符'],
+        // 密码必须是6位数字
+        pwd: function (value,item) { 
+            // 形参value表示输入域的值
+            // item表示DOM元素
+            // 验证6位数字
+            var reg = /^\d{6}$/;
+            // 如果规则不匹配就返回提示
+            if (!reg.test(value)) { 
+                return '密码必须是6位数字';
+            }
+        }
+    });
+
     // 控制表单的提交
     $('.layui-form').submit(function (e) {
         // 阻止表单默认提交行为
@@ -6,7 +25,7 @@ $(function () {
         // 获取表单输入域的用户名和密码
         var formData = $(this).serialize();
         // 提交表单之前需要做表单验证，如果自己实现有点繁琐，所以借助LayUI实现
-        console.log(formData)
+        
         // 调用后台接口验证是否正确
         $.ajax({
             type: 'post',
