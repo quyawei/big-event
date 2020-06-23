133d03ca393b8ce61e272a70edc3c11aaaab0fb8
quyawei <quyawei1@outlook.com>
2020-06-24 00:17:41 +0800

    添加备注

--- assets/js/my/my-basic.js
+++ assets/js/my/my-basic.js
@@ -14,7 +14,8 @@ $(function () {
                 // $('#form input[name=email]').val(res.data.email);
                 // 基于layui方式填充表单
                 // basicForm是form标签lay-filter属性值，用于表单数据填充
-                form.val('basicForm',res.data);
+                // form.val是layui提供的方法
+                form.val('basicForm', res.data);
             }
         });
     };
