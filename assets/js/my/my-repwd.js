e0f268eb85767b9a3f0c09c20398cd885fa10d6a
quyawei <quyawei1@outlook.com>
2020-06-24 21:36:15 +0800

    实现修改密码

--- assets/js/my/my-repwd.js
+++ assets/js/my/my-repwd.js
@@ -1,3 +1,20 @@
-$(function () { 
-    
-})
+$(function () {
+    // 控制表单提交
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        // 获取表单数据
+        var fd = $(this).serialize();
+        // 调用接口修改密码
+        $.ajax({
+            type: 'post',
+            url: 'my/updatepwd',
+            data: fd,
+            success: function (res) {
+                // 更新成功后，提示
+                if (res.status === 0) {
+                    layer.msg(res.message);
+                };
+            }
+        });
+    });
+});
