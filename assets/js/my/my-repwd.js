8a8cc120d03ac8ab8ed1d9e8d5e70f15828dcaa8
quyawei <quyawei1@outlook.com>
2020-06-24 21:52:46 +0800

    修改密码表单验证

--- assets/js/my/my-repwd.js
+++ assets/js/my/my-repwd.js
@@ -1,4 +1,27 @@
 $(function () {
+    // 处理表单验证
+    var form = layui.form;
+    // 自定义验证规则 
+    // 1.新密码和原来的密码不能一样
+    // 2.确认密码必须一致
+    form.verify({
+        diff: function (value) { 
+            // 1.新密码和原来的密码不能一样
+            var oldPwd = $('#form input[name=oldPwd]').val();
+            if (oldPwd === value) { 
+                return '新密码不能和原密码相同';
+            };
+        },
+        same: function (value) { 
+            // 2.确认密码必须一致
+            // 获取新密码
+            var newPwd = $('#form input[name=newPwd]').val();
+            if (newPwd !== value) { 
+                return '两次输入的密码不一致';
+            };
+        }
+    })
+    
     // 控制表单提交
     $('#form').submit(function (e) {
         e.preventDefault();
