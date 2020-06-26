fb63666f5f3cecb6fbe1e828de521858308aa408
quyawei <quyawei1@outlook.com>
2020-06-26 17:29:04 +0800

    common.js代码优化

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,40 +1,44 @@
-/*
+$(function () {
+    /*
     通用的接口调用设置
-*/
-var baseURL = 'http://ajax.frontend.itheima.net/';
-$.ajaxPrefilter(function (option) {
-    // 形参option是jQuery请求方法的配置信息
-    // 发送请求之前会触发beforeSend
-    option.beforeSend = function () {
-        // 发送请求之前发送进度条(添加window防止报错)
-        window.NProgress && window.NProgress.start();
-        // if (NProgress) { 
-        //     NProgress.start();
-        // };
-    };
+    */
+    var baseURL = 'http://ajax.frontend.itheima.net/';
+    
+    // 所有的请求发送之前经过该函数，可以对相关请求和响应做一些调整
+    $.ajaxPrefilter(function (option) {
+        // 形参option是jQuery请求方法的配置信息
+        // 发送请求之前会触发beforeSend
+        option.beforeSend = function () {
+            // 发送请求之前发送进度条(添加window防止报错)
+            window.NProgress && window.NProgress.start();
+            // if (NProgress) { 
+            //     NProgress.start();
+            // };
+        };
 
-    // 1.配置通用的URL地址
-    option.url = baseURL + option.url;
-    // 2.设置接口的通用请求头信息
-    if (option.url.lastIndexOf('/my/') !== -1) { 
-        // 所有包含my的请求路径需要进行权限验证(需要先登录)
-        // header默认不存在，所以需要设置一个对象
-        option.headers = {
-            Authorization: localStorage.getItem('mytoken')
+        // 1.配置通用的URL地址
+        option.url = baseURL + option.url;
+        // 2.设置接口的通用请求头信息
+        if (option.url.lastIndexOf('/my/') !== -1) {
+            // 所有包含my的请求路径需要进行权限验证(需要先登录)
+            // header默认不存在，所以需要设置一个对象
+            option.headers = {
+                Authorization: localStorage.getItem('mytoken')
+            };
         };
-    };
-    // 3.处理通用的异常情况
-    // 服务器响应结束时触发
-    option.complete = function (res) {
-        // 完成请求后，结束进度条
-        window.NProgress && window.NProgress.done();
-        
-        // 处理失败的情况
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') { 
-            // 如果身份验证失败了，就跳转到登录页
-            // 把无效token清除
-            localStorage.removeItem('mytoken');
-            location.href = './login.html';
+        // 3.处理通用的异常情况
+        // 服务器响应结束时触发
+        option.complete = function (res) {
+            // 完成请求后，结束进度条
+            window.NProgress && window.NProgress.done();
+
+            // 处理失败的情况
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 如果身份验证失败了，就跳转到登录页
+                // 把无效token清除
+                localStorage.removeItem('mytoken');
+                location.href = './login.html';
+            };
         };
-    };
+    });
 });
