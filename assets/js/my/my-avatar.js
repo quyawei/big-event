abb3269a85d4f01989b09efd9faddf7031ff4369
quyawei <quyawei1@outlook.com>
2020-06-26 18:12:52 +0800

    基本裁剪效果

--- assets/js/my/my-avatar.js
+++ assets/js/my/my-avatar.js
@@ -1,3 +1,9 @@
 $(function () {
-    
+    // 实现裁剪基本初始化效果
+    $('.cropper-box img').cropper({
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    });
 });
