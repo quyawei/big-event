9f26dd56ab99f0530e57290401eb7c6183ad270c
quyawei <quyawei1@outlook.com>
2020-06-27 16:56:12 +0800

    获取分类列表数据并渲染

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -1,3 +1,15 @@
 $(function () {
-
+    // 加载分类列表数据
+    function loadListData () { 
+        $.ajax({
+            type: 'get',
+            url: 'my/article/cates',
+            success: function (res) {
+                // 基于模板引擎渲染列表数据
+                var result = template('list-tpl', res);
+                $('.layui-table tbody').html(result);
+            }
+        });
+    };
+    loadListData();
 });
