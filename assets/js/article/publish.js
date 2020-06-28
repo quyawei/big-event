d1aaed79dd0413a953c7318bb306652e261e1d3b
quyawei <quyawei1@outlook.com>
2020-06-29 00:32:17 +0800

    发布文章提交操作

--- assets/js/article/publish.js
+++ assets/js/article/publish.js
@@ -1,3 +1,23 @@
 $(function () {
-    
+    // 绑定表单提交事件
+    $('#add-form').submit(function (e) {
+        e.preventDefault();
+        // var fd = $(this).serialize();
+        // 处理文件上传(this指的是谁？表单元素)
+        var fd = new FormData(this);
+        $.ajax({
+            type: 'post',
+            url: 'my/article/add',
+            data: fd,
+            // 防止把请求参数转换为字符串
+            processData: false,
+            // 禁止使用默认的提交参数类型
+            contentType: false,
+            success: function (res) {
+                if (res.status === 0) {
+                    layer.msg(res.message);
+                }
+            }
+        });
+    });
 });
