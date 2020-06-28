f5c13ec4681ba53231d90bab7d948fdb7e14d57f
quyawei <quyawei1@outlook.com>
2020-06-28 21:59:11 +0800

    分类筛选数据的动态填充

--- assets/js/article/list.js
+++ assets/js/article/list.js
@@ -1,3 +1,19 @@
 $(function () {
-
+    // 获取表单对象
+    var form = layui.form;
+    // 获取所有的文章分类数据
+    function loadCateData() { 
+        $.ajax({
+            type: 'get',
+            url: 'my/article/cates',
+            success: function (res) {
+                // 基于模板引擎渲染分类列表数据
+                var tags = template('cate-tpl', res);
+                $('#category').html(tags);
+                // 更新渲染select(layui提供的方法)
+                form.render('select');
+            }
+        });
+    };
+    loadCateData();
 });
